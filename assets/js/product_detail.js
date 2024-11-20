4d9abc69286819de33ce02ff702303f3c6179590
songu123 <vans72092@gmail.com>
2024-11-20 19:20:13 +0700

    feat: copy from project Web_Bootstrap

--- assets/js/product_detail.js
+++ assets/js/product_detail.js
@@ -14,7 +14,7 @@ renderProducts(product_Id);
 
 
 // Lấy dữ liệu giỏ hàng từ localStorage
-loadProductDetail(product_Id)
+loadProductDetail(product_Id);
 // Lấy dữ liệu sản phẩm từ localStorage
 
 // Hàm lấy số lượng sản phẩm trong giỏ hàng
@@ -59,9 +59,11 @@ function displaySubCart() {
   const poloProducts = JSON.parse(localStorage.getItem('polo')) || [];
   const aothunProducts = JSON.parse(localStorage.getItem('aothun')) || [];
   const quanthunProducts = JSON.parse(localStorage.getItem('quanthun')) || [];
+  const quankakiProducts = JSON.parse(localStorage.getItem('kaki')) || [];
   const quanlotProducts = JSON.parse(localStorage.getItem('brief')) || [];
+  const boxerProducts = JSON.parse(localStorage.getItem('boxer')) || [];
 
-  const allProducts = poloProducts.concat(aothunProducts, quanthunProducts, quanlotProducts);
+  const allProducts = poloProducts.concat(poloProducts, boxerProducts, aothunProducts, quanthunProducts, quankakiProducts, quanlotProducts);
   let tongtien = 0; // Changed from const to let
 
   const cart_list = document.querySelector(".cart-list");
@@ -182,9 +184,11 @@ function loadProductDetail(id) {
   const poloProducts = JSON.parse(localStorage.getItem('polo')) || [];
   const aothunProducts = JSON.parse(localStorage.getItem('aothun')) || [];
   const quanthunProducts = JSON.parse(localStorage.getItem('quanthun')) || [];
+  const quankakiProducts = JSON.parse(localStorage.getItem('kaki')) || [];
   const quanlotProducts = JSON.parse(localStorage.getItem('brief')) || [];
+  const boxerProducts = JSON.parse(localStorage.getItem('boxer')) || [];
 
-  const allProducts = poloProducts.concat(aothunProducts, quanthunProducts, quanlotProducts);
+  const allProducts = poloProducts.concat(poloProducts, boxerProducts, aothunProducts, quankakiProducts, quanthunProducts, quanlotProducts);
 
   const container_detail = document.getElementById("detail_container");
 
@@ -392,50 +396,34 @@ function renderProducts(id) {
   productContainer.innerHTML = '';
   products.forEach(product => {
     productContainer.innerHTML += `
-    <div class="col-md-3 px-3 my-3" style="">
-                    <a href="./product_detail.html?id=${product.id}" class="text-decoration-none">
-                        <div class="card product-card shadow-sm">
-                            <div class="position-relative overflow-hidden">
-                                <div class="img-container w-100">
-                                    <img src="${product.img1}" class="card-img-top"
-                                        alt="Product Image">
-                                    <img src="${product.img2}"
-                                        class="position-absolute card-img-bottom top-0" alt="Product Image">
-                                    <div
-                                        class="position-absolute bottom-0 start-0 text-center w-100 bg-dark text-white p-2 quick-view">
-                                        QUICK VIEW
-                                    </div>
-                                    <button class="loving-product position-absolute top-0 end-0 rounded-circle">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15"
-                                            fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
-                                            <path fill-rule="evenodd"
-                                                d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314" />
-                                        </svg>
-                                    </button>
-                                </div>
-                            </div>
-                            <div class="card-body">
-                                <div class="">
-                                    <h5 class="card-title">${product.name}</h5>
-                                    <p class="price fw-bold">${product.price} ₫</p>
-                                </div>
-                                <div class="">
-                                    <div class="d-flex">
-                                        <p class="text-muted">Màu sắc</p>
-                                        <div class="d-flex ps-2">
-                                            <img src="./assets/images/products/image.jpg" alt="color1"
-                                                class="rounded-circle me-2" width="30" height="30">
-                                            <img src="./assets/images/products/z5018069269739-aa5c5764d2d4602bb8b55ec50edad312-600x600.jpg"
-                                                alt="color2" class="rounded-circle me-2" width="30" height="30">
-                                            <img src="./assets/images/products/image.jpg" alt="color3"
-                                                class="rounded-circle" width="30" height="30">
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>`
+    <div class="col-sm-6 col-md-4 col-lg-3 px-3 mt-4 slick-slide slick-active" data-slick-index="3" aria-hidden="false" tabindex="-1" style="width: 332px;">
+        <a href="./product_detail.html?id=${product.id}" class="text-decoration-none" tabindex="0">
+          <div class="card product-card shadow-sm">
+            <div class="position-relative overflow-hidden">
+              <div class="img-container w-100">
+                <img src="${product.img1}" class="h-100 card-img-top" alt="Product Image">
+                <img src="${product.img2}" class="position-absolute h-100 card-img-bottom start-0 top-0" alt="Product Image">
+                <div class="position-absolute bottom-0 start-0 text-center w-100 bg-dark text-white p-2 quick-view">
+                  QUICK VIEW
+                </div>
+                <button class="loving-product position-absolute top-0 end-0 rounded-circle" tabindex="0">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"></path>
+                  </svg>
+                </button>
+              </div>
+            </div>
+            <div class="card-body">
+              
+    
+              <div class="">
+                <span class="card-title">${product.name}</span>
+                <p class="price fw-bold">${product.price} ₫</p>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>`
   })
 }
 
@@ -443,19 +431,25 @@ function findProductCategory(productId) {
   const poloProducts = JSON.parse(localStorage.getItem('polo')) || [];
   const aothunProducts = JSON.parse(localStorage.getItem('aothun')) || [];
   const quanthunProducts = JSON.parse(localStorage.getItem('quanthun')) || [];
-  const quanlotProducts = JSON.parse(localStorage.getItem('brief')) || [];
+  const quankakiProducts = JSON.parse(localStorage.getItem('kaki')) || [];
+  const briefProducts = JSON.parse(localStorage.getItem('brief')) || [];
+  const boxerProducts = JSON.parse(localStorage.getItem('boxer')) || [];
 
   if (poloProducts.some(product => product.id === productId)) {
     return poloProducts;
   } else if (aothunProducts.some(product => product.id === productId)) {
     return aothunProducts;
+  } else if (quankakiProducts.some(product => product.id === productId)) {
+    return quankakiProducts;
   } else if (quanthunProducts.some(product => product.id === productId)) {
     return quanthunProducts;
-  } else if (quanlotProducts.some(product => product.id === productId)) {
-  return quanlotProducts;
-} else {
-  return 'Product not found in any category';
-}
+  } else if (boxerProducts.some(product => product.id === productId)) {
+    return boxerProducts;
+  } else if (briefProducts.some(product => product.id === productId)) {
+    return briefProducts;
+  } else {
+    return 'Product not found in any category';
+  }
 }
 
 // Ví dụ sử dụng:
